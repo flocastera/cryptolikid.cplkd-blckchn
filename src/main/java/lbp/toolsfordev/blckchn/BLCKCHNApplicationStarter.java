1bd4e1b845b0d60ba79db7349ff020127de0bac7
Florian CASTERA <florian.castera@accenture.com>
2018-06-01 17:59:44 +0200

    FCA-EERMV - 01/06/2018 17:59 : Ajout main

--- src/main/java/lbp/toolsfordev/blckchn/BLCKCHNApplicationStarter.java
+++ src/main/java/lbp/toolsfordev/blckchn/BLCKCHNApplicationStarter.java
@@ -4,16 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Main
+ * BLCKCHNApplicationStarter
  *
  * @author xekg473
  * @version 1.0
  * @date 01/06/2018
  */
 @SpringBootApplication
-public class Main {
+public class BLCKCHNApplicationStarter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+		SpringApplication.run(BLCKCHNApplicationStarter.class, args);
 	}
+	
 }
